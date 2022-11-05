faa067488855a8a74c000e8e14a78f92764f08b8
kondansatoragah1 <117503654+kondansatoragah1@users.noreply.github.com>
2022-11-05 09:12:16 +0300

    Add files via upload

---
+++ 0,1 Çevirmece.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  5 09:00:12 2022
+
+@author: Acer
+"""
+
+while True:
+    a=input("Please enter your value...")
+    liste=[]
+    for i in str(a):
+        liste.append(i)
+    for i in liste:
+        if i=="a":
+            print("1")
+        elif i=="e":
+            print("1")
+        elif i=="ı":
+            print("1")
+        elif i=="i":
+            print("1")
+        elif i=="o":
+            print("1")
+        elif i=="ö":
+            print("1")
+        elif i=="u":
+            print("1")
+        elif i=="ü":
+            print("1")
+        elif i=="A":
+            print("1")
+        elif i=="E":
+            print("1")
+        elif i=="I":
+            print("1")
+        elif i=="İ":
+            print("1")
+        elif i=="O":
+            print("1")
+        elif i=="Ö":
+            print("1")
+        elif i=="U":
+            print("1")
+        elif i=="Ü":
+            print("1")
+        else:
+            print("0")
